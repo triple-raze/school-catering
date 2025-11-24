2eef456b4562067128ed00d7ec8e5b182d10f20e
triple-raze <weebucks1@gmail.com>
2025-11-24 12:04:32 +0300

    Delete backend/src directory

--- backend/src/models/class.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-class ClassModel(BaseModel):
-    id: int
-    teacher_id: int
-    digit: int = Field(max_digits=2)
-    letter: str = Field(max_length=1)
-
